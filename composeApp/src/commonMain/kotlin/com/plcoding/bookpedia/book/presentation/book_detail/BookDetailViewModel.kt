30b64c435eac751ff11e37210fc4ffc39c118657
tamaspenzes <tamas.penzes.con@hotstar.com>
2026-06-04 12:27:05 +0200

    details finished

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailViewModel.kt
@@ -1,19 +1,38 @@
 package com.plcoding.bookpedia.book.presentation.book_detail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import com.plcoding.bookpedia.app.Routes
+import com.plcoding.bookpedia.book.domain.BookRepository
+import com.plcoding.bookpedia.core.domain.onSuccess
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class BookDetailViewModel: ViewModel() {
+class BookDetailViewModel(
+    private val bookRepository: BookRepository,
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    private val bookId = savedStateHandle.toRoute<Routes.BookDetails>().id
     private val _state = MutableStateFlow<BookDetailState>(BookDetailState())
-    val state = _state.asStateFlow()
+    val state = _state.onStart {
+        fetchBookDescription()
+    }.stateIn(viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        _state.value)
 
     fun onAction(action: BookDetailAction) {
         when (action) {
             is BookDetailAction.OnFavouriteClick -> {
 
             }
+
             is BookDetailAction.OnSelectedBookChange -> {
                 _state.update {
                     it.copy(
@@ -21,7 +40,24 @@ class BookDetailViewModel: ViewModel() {
                     )
                 }
             }
+
             else -> Unit
+        }
+    }
+
+    fun fetchBookDescription() {
+        viewModelScope.launch {
+            bookRepository.getBookDescription(bookId)
+                .onSuccess { description ->
+                    _state.update {
+                        it.copy(
+                            book = it.book?.copy(
+                                description = description
+                            ),
+                            isLoading = false
+                        )
+                    }
+                }
         }
     }
 }
